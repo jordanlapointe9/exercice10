708d2f9fe81d4b61d808241e256761a91d43e865
Jordan Lapointe <jordan.lapointe87@gmail.com>
2020-03-06 15:53:31 -0500

    Finition du code concernant le bouton Lire plus

--- js/main.js
+++ js/main.js
@@ -9,29 +9,47 @@ if (collectionBtnNouvelle) {
 
 function Ajax(evt) {
   let evtId = evt.target.id;
+  let evtDataSet = evt.target.dataset.checked;
 
-  let maRequete = new XMLHttpRequest();
-  maRequete.open(
-    "GET",
-    "http://localhost/exercice9/wp-json/wp/v2/posts/" + evtId
-  );
+  let evtContentId = "#content-" + evtId;
+  let contenuDiv = document.querySelector(evtContentId);
+  let collectionBtn = document.querySelectorAll(".button-article");
 
-  maRequete.onload = function() {
-    if (maRequete.status >= 200 && maRequete.status < 400) {
-      let data = JSON.parse(maRequete.responseText);
-      console.log(evt.target.dataset.checked);
-      // instructions ici
-      creationHTML(data); // paramètres à ajouter
-    } else {
-      console.log("La connexion est faite mais il y a une erreur");
+  for (let i = 0; i < collectionBtn.length; i++) {
+    if(collectionBtn[i].dataset.checked == "true"){
+      const localContenuDiv = document.querySelector("#content-" + collectionBtn[i].id);
+      localContenuDiv.innerHTML = "";
     }
-  };
+  }
 
-  maRequete.onerror = function() {
-    console.log("erreur de connexion");
-  };
+  if (evtDataSet == "false") {
+    evt.target.dataset.checked = "true";
 
-  maRequete.send();
+    let maRequete = new XMLHttpRequest();
+    maRequete.open(
+      "GET",
+      "http://localhost/exercice9/wp-json/wp/v2/posts/" + evtId
+    );
+
+    maRequete.onload = function() {
+      if (maRequete.status >= 200 && maRequete.status < 400) {
+        let data = JSON.parse(maRequete.responseText);
+
+        creationHTML(data); // paramètres à ajouter
+      } else {
+        console.log("La connexion est faite mais il y a une erreur");
+      }
+    };
+
+    maRequete.onerror = function() {
+      console.log("erreur de connexion");
+    };
+
+    maRequete.send();
+  } else {
+    contenuDiv.innerHTML = "";
+    evt.target.dataset.checked = "false";
+  }
 }
 ///////////////////////////////////////////////////////
 
